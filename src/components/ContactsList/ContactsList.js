3f33119d2293cef33c15e3594e4cfae649e1aa71
VladKapusta <vladkapysta96@gmail.com>
2023-02-06 23:08:01 +0200

    1

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,21 +1,35 @@
 import { ContactItem } from './ContactItem/ContactItem';
-import { ContactsBox, ListContacts, Contact } from './contactsList.styled';
+// import { ContactsBox, ListContacts, Contact } from './contactsList.styled';
 
 export const ContactsList = ({ contacts, deleteContact }) => {
   return (
-    <ContactsBox>
-      <ListContacts>
-        {contacts.map(({ id, name, number }) => (
-          <Contact key={id}>
-            <ContactItem
-              name={name}
-              number={number}
-              id={id}
-              deleteContact={deleteContact}
-            />
-          </Contact>
-        ))}
-      </ListContacts>
-    </ContactsBox>
+    <div>
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <ContactItem
+            name={name}
+            number={number}
+            id={id}
+            deleteContact={deleteContact}
+          />
+        </li>
+      ))}
+    </ul>
+  </div>
+    // <ContactsBox>
+    //   <ListContacts>
+    //     {contacts.map(({ id, name, number }) => (
+    //       <Contact key={id}>
+    //         <ContactItem
+    //           name={name}
+    //           number={number}
+    //           id={id}
+    //           deleteContact={deleteContact}
+    //         />
+    //       </Contact>
+    //     ))}
+    //   </ListContacts>
+    // </ContactsBox>
   );
 };
